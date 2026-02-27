ab6d531349933a15931ac9b2d6638a7df417e5ad
Muhammad Ramzan Abid <135987304+muhammadramzan440@users.noreply.github.com>
2026-02-27 17:36:31 +0000

    Refactor object examples and enhance comments for clarity

--- obj.js
+++ obj.js
@@ -14,4 +14,35 @@ const objuser = {
 
 // destructuring of objects 
 const {fullname:name}= objuser;
-console.log(name);
+// console.log(name);
+ // defining another object 
+
+const objuser2 = {
+
+
+};
+ objuser2.fullname = "Jane";
+ objuser2.age = 25;
+ objuser2.city = "Los Angeles";
+//  console.log(objuser2);
+ 
+
+ // combining two objects using Object.assign() method
+ const combinedObj = Object.assign({}, objuser, objuser2);
+ console.log(combinedObj);
+
+//  using spread operator 
+const combinedObj2 = {...objuser,...objuser2};
+// console.log(combinedObj2);
+
+
+
+//extracting keys and values from an object using Object.keys() and Object.values() methods
+const keys = Object.keys(objuser);
+const values = Object.values(objuser);
+// console.log(keys);
+// console.log(values);
+
+// checking if a property exists in an object using hasOwnProperty() method
+// console.log(objuser.hasOwnProperty("fullname")); // true
+// console.log(objuser.hasOwnProperty("email")); // false
