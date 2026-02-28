b9540396b64058123687595af53893324e1d363c
Muhammad Ramzan Abid <135987304+muhammadramzan440@users.noreply.github.com>
2026-02-28 08:37:53 +0000

    Add functions for sum, product, and user login validation

---
+++ functions.js
@@ -0,0 +1,30 @@
+function calculateSum(a, b) {
+  return a + b;
+}
+
+const result = calculateSum(10, 5); 
+// console.log(result);
+
+function calculateProduct(a, b) {
+  return a * b;
+}
+
+const productResult = calculateProduct(10, 5); 
+// console.log(productResult);
+
+
+
+
+
+ function IsLoggedIn(username){
+    if(!username){
+
+        return(`please provide a username`);
+    }
+    return(`Welcome, ${username}! You are logged in.`);
+ }
+console.log(IsLoggedIn("Muhammad Ramzan Abid"));
+
+
+
+
